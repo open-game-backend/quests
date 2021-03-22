7d517afb01abab7c83896cabf0a7862a7f56d706
Nick Prühs <dev@npruehs.de>
2021-03-22 18:44:16 +0100

    Add initial project files.

---
+++ src/main/java/de/opengamebackend/quests/QuestsApplication.java
@@ -0,0 +1,13 @@
+package de.opengamebackend.quests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/opengamebackend/quests/QuestsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.opengamebackend.quests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
