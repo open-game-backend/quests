74ac11ac58e8686522c4a7cd2be2ea78803eb0b3
Nick Prühs <dev@npruehs.de>
2021-03-23 14:53:40 +0100

    Set up logging.

--- src/main/java/de/opengamebackend/quests/QuestsApplication.java
+++ src/main/java/de/opengamebackend/quests/QuestsApplication.java
@@ -1,9 +1,13 @@
 package de.opengamebackend.quests;
 
+import de.opengamebackend.util.EnableOpenGameBackendUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@EnableOpenGameBackendUtils
+@ConfigurationPropertiesScan
 public class QuestsApplication {
 
 	public static void main(String[] args) {
